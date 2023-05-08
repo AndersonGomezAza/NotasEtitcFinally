1dbe78428823d3d238f0235c76839341b575113b
AndersonGomezAza <argomeza@itc.edu.co>
2023-05-08 12:59:17 -0500

    first commit 2

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const nota3 = d.getElementById('Nota3');
 const notaDefinitiva = d.getElementById('NotaDefinitiva');
 let resultado3Corte = 0, resultadoFinal = 0, resultadoFinal3Corte = 0;
 
-btnNota.addEventListener('click', ()=>{
+btnNota.addEventListener('click', (e)=>{
   resultado3Corte = ((nota1.value*1.75)/5.0)+((nota2.value*1.75)/5.0);
   resultadoFinal3Corte = ((3.0-resultado3Corte)*5.0)/1.5;
   nota3.value = resultadoFinal3Corte;
+  e.preventDefault();
 })
 
-btnDefinitiva.addEventListener('click', ()=>{
+btnDefinitiva.addEventListener('click', (e)=>{
   resultadoFinal = ((nota1.value*1.75)/5.0)+((nota2.value*1.75)/5.0)+((nota3.value*1.5)/5.0);
   notaDefinitiva.value = resultadoFinal;
+  e.preventDefault();
 })
